5d18ff525b6f96c9488353f346a63bdd9b4935f2
ТвоёИмя <@.com>
2026-09-13 20:57:05 +0300

    feat: добавлена картинка для VK Stories через Vercel public folder
    - Добавлен story-bg.png в папку public
    - Обновлена функция shareToStory с прямой ссылкой на Vercel
    - Исправлены параметры для VKWebAppShowStoryBox

--- src/components/RadioPlayer.tsx
+++ src/components/RadioPlayer.tsx
@@ -447,8 +447,8 @@ export const RadioPlayer: React.FC<RadioPlayerProps> = ({ id }) => {
     // === ПОДЕЛИТЬСЯ В ИСТОРИИ VK (с готовой картинкой) ===
     const shareToStory = async () => {
         try {
-            // Прямая ссылка
-            const imageUrl = 'https://disk.yandex.ru/client/disk?idApp=client&dialog=slider&idDialog=%2Fdisk%2Fstory-bg.png';
+
+            const imageUrl = '/story-bg.png';
 
             await bridge.send('VKWebAppShowStoryBox', {
                 background_type: 'image',
